76794342642fb2a64fba8902fdb873114e526411
schumi0 <111157903+schumi0@users.noreply.github.com>
2024-06-05 09:31:26 +0200

    Commit for plugins fetcher
    getting plugins should be working for if given os/path and if not

--- gui/backend/plugins_manager.py
+++ gui/backend/plugins_manager.py
@@ -1,25 +1,17 @@
 import os
+from volatility_finder import find_volatility_file
 
-def find_volatility_file(start_path, max_attempts=5):
-    current_path = start_path
-    attempts = 0
 
-    while attempts < max_attempts:
-        if 'vol.py' in os.listdir(current_path):
-            return os.path.join(current_path, 'vol.py')
 
-        current_path = os.path.dirname(current_path)
-        attempts += 1
-
-    return None
-
-def get_all_plugins():
-    """Return a list of available plugins for different operating systems."""
+def get_all_plugins(filepath=None, os_name=None):
     try:
         start_path = os.path.dirname(os.path.realpath(__file__))
-        volatility_file = find_volatility_file(start_path)
+        volatility_file = find_volatility_file(start_path) if filepath is None else filepath
+        
+        if volatility_file is None:
+            print("Volatility file not found.")
+            return []
 
-        # Explicitly set the base directory to the correct path
         base_dir = os.path.dirname(volatility_file)
 
         plugin_directories = {
@@ -27,11 +19,24 @@ def get_all_plugins():
             'Linux': os.path.join(base_dir, 'volatility3', 'framework', 'plugins', 'linux'),
             'Mac': os.path.join(base_dir, 'volatility3', 'framework', 'plugins', 'mac')
         }
-
         plugin_list = []
 
-        for os_name, dir_path in plugin_directories.items():
-            if os.path.exists(dir_path) and os.path.isdir(dir_path):
+        print(plugin_list)
+
+        if os_name is None:
+            print("OS is undefined.")
+            for os_name, dir_path in plugin_directories.items():
+                if os.path.exists(dir_path) and os.path.isdir(dir_path):
+                    plugins = [f"{os_name.lower()}.{os.path.splitext(f)[0]}" for f in os.listdir(dir_path) if
+                               os.path.isfile(os.path.join(dir_path, f)) and f.endswith('.py')]
+                    plugin_list.append((os_name, plugins))
+                else:
+                    print(f"Directory {dir_path} does not exist or is not a directory.")
+        else:
+            print("OS is defined. {os_name}")
+            dir_path = plugin_directories.get(os_name)
+            if dir_path is not None and os.path.exists(dir_path) and os.path.isdir(dir_path):
+                print(f"Directory {dir_path} exists and is a directory.")
                 plugins = [f"{os_name.lower()}.{os.path.splitext(f)[0]}" for f in os.listdir(dir_path) if
                            os.path.isfile(os.path.join(dir_path, f)) and f.endswith('.py')]
                 plugin_list.append((os_name, plugins))
