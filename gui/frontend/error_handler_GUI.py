828120106212d5ceb21848c9800bfe3885a859a5
D1dude <114227036+D1dude@users.noreply.github.com>
2024-06-13 10:24:19 +0200

    comment update 4 back

--- gui/frontend/error_handler_GUI.py
+++ gui/frontend/error_handler_GUI.py
@@ -1,8 +1,8 @@
 from PyQt5.QtWidgets import QMessageBox
 
 
+# display an error message in a QMessageBox
 def show_error_message(parent, title, message):
-    # display an error message in a QMessageBox
     msg_box = QMessageBox(parent)
     msg_box.setIcon(QMessageBox.Critical)
     msg_box.setWindowTitle(title)
